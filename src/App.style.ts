93e3db2e1b0b0f507663c9e692cd830b72cbd95a
Jinnie <hjk7639@gmail.com>
2023-03-26 15:07:43 +0900

    루트 section에 스타일 적용 & 루트 스타일 파일 생성

---
+++ src/App.style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const SectionStyle = styled.section`
+  width: 800px;
+  height: 100vh;
+  margin: 0 auto;
+  text-align: center;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import AgeInfo from './component/AgeInfo';
+import BirthForm from './component/BirthForm';
+import NameForm from './component/NameForm';
+
+import { SectionStyle } from './App.style';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <SectionStyle>
+      <h1>만 나이 계산기🧵</h1>
+      <NameForm />
+      <BirthForm />
+      <AgeInfo />
+    </SectionStyle>
+  );
 }
 
 export default App;
